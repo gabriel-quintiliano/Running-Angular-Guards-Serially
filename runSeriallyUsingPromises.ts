8557afd418fe8d4bd0dcb632f629e61649805e66
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2024-02-28 22:10:25 -0300

    Refactor runSerially function (run with promises) to evaluate less conditionals overall

--- runSeriallyUsingPromises.ts
+++ runSeriallyUsingPromises.ts
@@ -1,7 +1,7 @@
 import { CanActivateChildFn, CanActivateFn, UrlTree } from '@angular/router';
 import { Observable, lastValueFrom, take } from 'rxjs';
 
-export function runSeriallyWithPromisesAny(...guards: CanActivateFn[] | CanActivateChildFn[]): CanActivateFn | CanActivateChildFn {
+export function runSerially(...guards: CanActivateFn[] | CanActivateChildFn[]): CanActivateFn | CanActivateChildFn {
 
     return (route, state) => {
         return new Promise(resolve => {
@@ -14,33 +14,30 @@ export function runSeriallyWithPromisesAny(...guards: CanActivateFn[] | CanActiv
 
                 for (index; index < guards.length; index++) {
                     const guard = guards[index];
-                    const guardReturn = guard(route, state);
+                    let guardReturn = guard(route, state);
 
-                    if (!guardReturn || guardReturn instanceof UrlTree) {
+                    if (guardReturn === true) {
+                        continue;
+                    } else if (!guardReturn || guardReturn instanceof UrlTree) {
                         resolve(guardReturn);
                         return;
-
                     } else if (guardReturn instanceof Observable) {
-                        lastValueFrom(guardReturn.pipe(take(1)))
-                        .then(handleAsyncRes(index))
-                        return;
-                    } else if (guardReturn instanceof Promise) {
-                        guardReturn.then(handleAsyncRes(index))
-                        return;
+                        guardReturn = lastValueFrom(guardReturn.pipe(take(1)))
                     }
+
+                    guardReturn.then(
+                        (res) => {
+                            if (!res || res instanceof UrlTree) {
+                                resolve(res);
+                            } else {
+                                parseGuardsStartingAt(index+1);
+                            }
+                        }
+                    )
+                    return;
                 }
 
                 resolve(true);
-            }
-
-            const handleAsyncRes = (currentIndex: number) => {
-                return (res: Boolean | UrlTree) => {
-                    if (!res || res instanceof UrlTree) {
-                        resolve(res);
-                    } else {
-                        parseGuardsStartingAt(currentIndex+1);
-                    }
-                }
             }
 
             parseGuardsStartingAt(0);
@@ -64,7 +61,7 @@ export function runSeriallyWithPromisesAny(...guards: CanActivateFn[] | CanActiv
  * Only if all guards return `true` the route will be activated.
  * 
  * Returns a single "wrapper" guard that is equivalent to all other guards passed as argument. */
-function _runSeriallyWithPromisesAny(...guards: CanActivateFn[] | CanActivateChildFn[]): CanActivateFn | CanActivateChildFn {
+export function _runSerially(...guards: CanActivateFn[] | CanActivateChildFn[]): CanActivateFn | CanActivateChildFn {
 
     // returns a "wrapper" guard that embeds all guards passed as arguments
     return (route, state) => {
@@ -88,66 +85,62 @@ function _runSeriallyWithPromisesAny(...guards: CanActivateFn[] | CanActivateChi
                 // 2. Start looping through guards stating at `index`
                 for (index; index < guards.length; index++) {
                     const guard = guards[index]; // 2.2 gets the guard reference.
-                    const guardReturn = guard(route, state); // 2.3 executes the guard and get its return value.
+                    let guardReturn = guard(route, state); // 2.3 executes the guard and get its return value.
 
-                    // 3. Check if `guardReturn` value is `false`, an UrlTree, Observable or Promise
-                    // If `guardReturn` value is `true` the loop will go on until it eventually finishes.
-                    if (!guardReturn || guardReturn instanceof UrlTree) {
-                        // 3.1 Checks if `guardReturn` value is either `false` or an UrlTree, if so resolves
+                    // 3. Check `guardReturn` actual value
+                    if (guardReturn === true) {
+                        // 3.1. Checks if `guardReturn` value is `true`, if so, skips the rest of this loop cycle.
+                        continue;
+                    } else if (!guardReturn || guardReturn instanceof UrlTree) {
+                        // 3.2 Checks if `guardReturn` value is either `false` or an UrlTree, if so resolves
                         // the Promise will resolve to this value right away.
                         resolve(guardReturn);
                         // As in the beginning of this helper function, it doesn't make much difference but
                         // it's better to have this return bellow.
                         return;
                     } else if (guardReturn instanceof Observable) {
-                        // 3.2 Checks if `guardReturn` value is an Observable, if so, the `take(1)` pipe
+                        // 3.3 Checks if `guardReturn` value is an Observable, if so, the `take(1)` pipe
                         // will be applied to make sure this observable completes after emiting 1 value and
                         // right after it will be converted to a Promise that resolve to the same value the
-                        // Observable would've emitted at first. As soon as this Promise resolves will handle
-                        // the response via the `handleAsyncRes()` helper function, `index` is passed as
-                        // argument in case there is any need to keep parsing next guards in the `guards` array.
-                        lastValueFrom(guardReturn.pipe(take(1)))
-                        .then(handleAsyncRes(index));
-                        // This return bellow is really important, as Promises run in the "backgound" the
-                        // function would keep running before the Promise above could resolve to any value,
-                        // and this way the loop would end and the main Promise would always resolve to `true`
-                        // even if this should not be the case and the still non resolved promise above
-                        // resolves to `false` or an UrlTree.
-                        return;
-                    } else if (guardReturn instanceof Promise) {
-                        // 3.3 Checks if `guardReturn` value is an Promise, if so, the same actions taken in the
-                        // condition above will be taken now.
-                        guardReturn.then(handleAsyncRes(index));
-                        return;
+                        // Observable would've emitted at first.
+                        guardReturn = lastValueFrom(guardReturn.pipe(take(1)))
                     }
+
+                    // 4. If the loop execution has reached this point, this means `guardReturn` is a Promise,
+                    // even if it was initially an Observable, it has been converted to a Promise above.
+                    // Now, As soon as this Promise resolves to a value (boolean | UrlTree), this value will be
+                    // handled accordingly.
+                    guardReturn.then(
+                        (res) => {
+                            if (!res || res instanceof UrlTree) {
+                                // 4.1 Checks if `res` (value resolved by the Promise) is `false` or an UrlTree, if
+                                // so the Promise returned by this "wrapper" guard will also resolve to `true`.
+                                resolve(res);
+                            } else {
+                                // 4.2 If `res` is `true`, the `parseGuardsStartingAt()` helper function will be
+                                // executed to keep parsing the next guards in the `guards` array. Remember,
+                                // if we're at the last guard, `index+1` will be equal to `guards` length,
+                                // thus thethe Promise returned by this "wrapper" guard will resolve to `true`
+                                // (within `parseGuardsStartingAt()`)
+                                parseGuardsStartingAt(index+1);
+                            }
+                        }
+                    )
+                    // This return bellow is really important, as Promises run in the "backgound", this function 
+                    // will keep running before the Promise above can resolve to any value, and this way the loop
+                    // would eventually end and the Promise returned by this "wrapper" guard would always resolve
+                    // to `true` even if this should not be the case and the still non resolved promise above
+                    // resolves to `false` or an UrlTree.
+                    return;
                 }
 
-                // 4. If the for loop has completed, this mean all the return values from all the guards in
+                // 5. If the for loop has completed, this mean all the return values from all the guards in
                 // the `guards` array were either `true` or a Promise or Observable that has resolved/emitted
                 // `true`, so the Promise returned by this "wrapper" guard will also resolve to `true`
                 resolve(true);
             }
 
-            // This helper function returns another function that will parse the value resolved by a Promise
-            // (or Observable converted to Promise) that was returned by a guard
-            const handleAsyncRes = (currentIndex: number) => {
-                return (res: Boolean | UrlTree) => {
-                    if (!res || res instanceof UrlTree) {
-                        // Checks if `res` (value resolved by the Promise) is `false` or an UrlTree, if so
-                        // the Promise returned by this "wrapper" guard will also resolve to `true`
-                        resolve(res);
-                    } else {
-                        // If `res` is `true`, the `parseGuardsStartingAt()` helper function will be executed
-                        // to keep parsing the next guards in the `guards` array.
-                        // Remember, if we're at the last guard, `currentIndex+1` will be equal to `guards`
-                        // length, thus thethe Promise returned by this "wrapper" guard will resolve to `true`
-                        // (within `parseGuardsStartingAt()`)
-                        parseGuardsStartingAt(currentIndex+1);
-                    }
-                }
-            }
-
-            // 5. Trigger the execution of `parseGuardsStartingAt()` beginning at index 0.
+            // 6. Trigger the execution of `parseGuardsStartingAt()` beginning at index 0.
             parseGuardsStartingAt(0);
         })
     }
